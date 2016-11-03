4050faf9ec34bc29b03b5161629226674b439725
kyuepublic <keeyue@gmail.com>
2016-11-03 10:52:45 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# setup(
+#     name='medfiltmp',
+#     version='0.1',
+#     # packages=find_packages('medfiltmp'),
+#     packages=['medfiltmp'],
+#     license='MIT',
+#     description='mp version of 3d filter',
+#     url='https://github.com',
+#     author='ke yue',
+#     author_email='kyue@aps.anl.gov',
+#     zip_safe=False)
+
+setup(name='medfiltmp',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['medfiltmp'],
+      zip_safe=False)
