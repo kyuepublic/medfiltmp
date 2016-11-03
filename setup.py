e84990b2308efaacf214b930254c5de70fbbaf79
Ke Yue <kyue@aps.anl.gov>
2016-11-03 15:40:47 -0500

    add

--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
 from setuptools import setup, find_packages
 
-# setup(
-#     name='medfiltmp',
-#     version='0.1',
-#     # packages=find_packages('medfiltmp'),
-#     packages=['medfiltmp'],
-#     license='MIT',
-#     description='mp version of 3d filter',
-#     url='https://github.com',
-#     author='ke yue',
-#     author_email='kyue@aps.anl.gov',
-#     zip_safe=False)
-
 setup(name='medfiltmp',
       version='0.1',
-      description='The funniest joke in the world',
+      description='mp version of 3d filter',
       url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      author='ke yue',
+      author_email='kyue@aps.anl.gov',
       license='MIT',
       packages=['medfiltmp'],
       zip_safe=False)
