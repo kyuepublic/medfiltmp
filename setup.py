30e34cccb420fb3da9d4bea53777602fe1b18e7e
Ke Yue <kyue@aps.anl.gov>
2016-11-04 15:57:53 -0500

    update conda configure

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='medfiltmp',
       version='0.1',
-      description='mp version of 3d filter',
-      url='http://github.com/storborg/funniest',
+      description='multi processing version of 3d filter',
+      url='https://github.com/kyuepublic/medfiltmp',
       author='ke yue',
       author_email='kyue@aps.anl.gov',
-      license='MIT',
+      license='BSD-3',
       packages=['medfiltmp'],
       zip_safe=False)
--- tests/test.py
+++ tests/test.py
@@ -11,24 +11,24 @@ from multiprocessing import Process, Array
 
 if __name__ == '__main__':
 
-        filter_size = 3
-        lensize = 320
+    filter_size = 3
+    lensize = 320
 
-        imsizex =lensize # image size for the input
-        imsizey = lensize
-        prjsize=lensize
+    imsizex =lensize # image size for the input
+    imsizey = lensize
+    prjsize=lensize
 
-        combinedMed = np.zeros(shape=(prjsize,imsizey,imsizex), dtype=np.float32)
-        for step in range (5,5+prjsize):
-            im_noise = np.arange( 10, imsizey*imsizex*step+10, step ).reshape(imsizey, imsizex)
-            im_noise = im_noise.astype(np.float32)
-            combinedMed[step-5]=im_noise
+    combinedMed = np.zeros(shape=(prjsize,imsizey,imsizex), dtype=np.float32)
+    for step in range (5,5+prjsize):
+        im_noise = np.arange( 10, imsizey*imsizex*step+10, step ).reshape(imsizey, imsizex)
+        im_noise = im_noise.astype(np.float32)
+        combinedMed[step-5]=im_noise
 
-        proj = combinedMed
+    proj = combinedMed
 
 
-        finalData = medfiltmp.median_filter(proj, filter_size)
+    finalData = medfiltmp.median_filter(proj, footprint=np.ones((filter_size, filter_size, filter_size)))
 
-        sfinaldata = ndimage.median_filter(proj,footprint=np.ones((filter_size, filter_size, filter_size)))
+    sfinaldata = ndimage.median_filter(proj,footprint=np.ones((filter_size, filter_size, filter_size)))
 
-        print("the result is ",sfinaldata-finalData)
+    print("the result is ",sfinaldata-finalData)
