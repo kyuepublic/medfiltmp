6df5c6c709c8fee406db5ffacf9acfa35f50ba57
kyuepublic <keeyue@gmail.com>
2018-02-19 14:16:34 -0600

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     imsizex =lensize # image size for the input
     imsizey = lensize
     prjsize=lensize
-
+    
+    #create a 3D test array
     combinedMed = np.zeros(shape=(prjsize,imsizey,imsizex), dtype=np.float32)
     for step in range (5,5+prjsize):
         im_noise = np.arange( 10, imsizey*imsizex*step+10, step ).reshape(imsizey, imsizex)
@@ -24,9 +25,11 @@ if __name__ == '__main__':
 
     proj = combinedMed
 
-
+    #median_filter in medfiltmp package
     finalData = medfiltmp.median_filter(proj, footprint=np.ones((filter_size, filter_size, filter_size)))
-
+    
+    #median_filter in ndimage package
     sfinaldata = ndimage.median_filter(proj,footprint=np.ones((filter_size, filter_size, filter_size)))
-
+    
+    #compare the result of median_filter from medfiltmp package and ndimage package.
     print("the result is ",sfinaldata-finalData)
